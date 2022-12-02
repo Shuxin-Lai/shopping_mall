f2ac7cbfd4df04cb58f971edc71ce4edcdc6b207
Shuxin-Lai <shuxinlai001@gmail.com>
2022-12-03 00:49:37 +0800

    chore: add swagger

--- v-mall-server/v-mall-api/src/main/java/com/vmall/Application.java
+++ v-mall-server/v-mall-api/src/main/java/com/vmall/Application.java
@@ -2,9 +2,10 @@ package com.vmall;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.vmall.*")
